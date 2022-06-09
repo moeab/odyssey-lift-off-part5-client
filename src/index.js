aaf387c7f6c98e9f69e429c9147aa55189d77ba6
Moe Ababneh <moe.ab.10@gmail.com>
2022-06-09 12:58:08 -0400

    uri fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://apollo-tut.herokuapp.com/',
+  uri: 'https://apollo-tuts-client.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
